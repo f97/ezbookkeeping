0b4a1d4a8ab5d15d683cbc0240d33edde86ce04c
Huỳnh Đức Khoản <f97.xyz@proton.me>
2024-12-04 09:00:24 +0700

    add axios-cache-interceptor

--- src/lib/services.js
+++ src/lib/services.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import Axios from 'axios';
+import { setupCache } from 'axios-cache-interceptor';
 
 import apiConstants from '@/consts/api.js';
 import userState from './userstate.js';
@@ -17,6 +18,9 @@ import { generateRandomUUID } from './misc.js';
 
 let needBlockRequest = false;
 let blockedRequests = [];
+
+const instance = Axios.create();
+const axios = setupCache(instance);
 
 axios.defaults.baseURL = apiConstants.baseApiUrlPath;
 axios.defaults.timeout = apiConstants.defaultTimeout;
